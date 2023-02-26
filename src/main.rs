3930c7a2d7a8a2797c95738d9059da7e2ef5ec3c
Björn Steinbrink <bsteinbr@gmail.com>
2023-02-26 21:12:29 +0100

    Setup a webserver with a basic health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    run(TcpListener::bind("127.1:0").expect("Failed to bind 127.1:8000."))?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,26 @@
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn health_check_works() {
+    let address = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(format!("{address}/health_check"))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.1:0").expect("Failed to bind random port on localhost.");
+
+    let port = listener.local_addr().unwrap().port();
+    tokio::spawn(zero2prod::run(listener).expect("Failed to bind address."));
+
+    format!("http://127.1:{port}")
+}

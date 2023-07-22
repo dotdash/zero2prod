ea22ba261081745587ceb90518c73443e9475c7d
Björn Steinbrink <bsteinbr@gmail.com>
2023-07-22 13:36:43 +0200

    Enable logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::net::TcpListener;
 
+use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let cfg = get_configuration().expect("Failed to read configuration.");
 
     let address = format!("127.1:{}", cfg.application_port);
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use actix_web::{dev::Server, web, App, HttpServer};
+use actix_web::{dev::Server, middleware::Logger, web, App, HttpServer};
 use sqlx::PgPool;
 
 use crate::routes;
@@ -10,6 +10,7 @@ pub fn run(listener: TcpListener, connection_pool: PgPool) -> std::io::Result<Se
 
     Ok(HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
             .app_data(connection_pool.clone())
